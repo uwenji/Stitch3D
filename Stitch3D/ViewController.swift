f4031035a40faa47cf940543fa120c71a2fe0c1b
You-Wen Ji <ohisyouwen.ji@gmail.com>
2019-08-31 14:41:55 +0200

    Initial Commit

---
+++ Stitch3D/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stitch3D
+//
+//  Created by You-Wen Ji on 8/31/19.
+//  Copyright © 2019 You-Wen Ji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
